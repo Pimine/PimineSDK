bd2a41f9d875ddb4a5a32cc48b6533b14fb5fef4
Денис Андрейчук <denis.andrei4uk@yandex.ua>
2020-05-20 20:11:37 +0300

    Utilities: Implement getTopViewController(base:)

--- PimineSDK/Utilities/PMUtilities.swift
+++ PimineSDK/Utilities/PMUtilities.swift
@@ -23,4 +23,26 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-import Foundation
+import UIKit
+
+final public class PMUtilities {
+    
+    static func getTopViewController(base: UIViewController) -> UIViewController {
+        // Navigation controller
+        if
+            let navigationController = base as? UINavigationController,
+            let visibleController = navigationController.visibleViewController {
+            return getTopViewController(base: visibleController)
+        // TabBar controller
+        } else if
+            let tabBarController = base as? UITabBarController,
+            let selectedController = tabBarController.selectedViewController {
+            return getTopViewController(base: selectedController)
+        // Default presented controller
+        } else if let presented = base.presentedViewController {
+            return getTopViewController(base: presented)
+        }
+        return base
+    }
+}
+
