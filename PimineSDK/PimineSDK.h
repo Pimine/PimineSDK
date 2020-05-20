d969af6f52b6f448b16f7f7a6d2a00170b9a5c5a
Денис Андрейчук <denis.andrei4uk@yandex.ua>
2020-05-20 19:27:11 +0300

    Development: Create SDK. Add SwifterSwift to submodule HandyExtensions

---
+++ PimineSDK/PimineSDK.h
@@ -0,0 +1,19 @@
+//
+//  PimineSDK.h
+//  PimineSDK
+//
+//  Created by Денис Андрейчук on 20.05.2020.
+//  Copyright © 2020 Pimine. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PimineSDK.
+FOUNDATION_EXPORT double PimineSDKVersionNumber;
+
+//! Project version string for PimineSDK.
+FOUNDATION_EXPORT const unsigned char PimineSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PimineSDK/PublicHeader.h>
+
+
