6c6f68555623df125d941f46fda4afe29734b991
Денис Андрейчук <denis.andrei4uk@yandex.ua>
2020-05-20 23:04:28 +0300

    Utilities: Remove unused modules.

--- Sources/Utilities/PMGeneralError.swift
+++ Sources/Utilities/PMGeneralError.swift
@@ -23,8 +23,6 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-import Foundation
-
 public struct PMGeneralError: LocalizedError {
     
     public let message: String
--- Sources/Utilities/PMesssages.swift
+++ Sources/Utilities/PMesssages.swift
@@ -23,8 +23,6 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //  SOFTWARE.
 
-import Foundation
-
 public struct PMesssages {
     
     static public let whoops = "Whoops"
