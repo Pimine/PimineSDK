1f54105c4e253e35c95f9c3614a5bb63ef3e25a3
Денис Андрейчук <denis.andrei4uk@yandex.ua>
2020-07-23 18:33:10 +0300

    SwiftyStore: Implement MerchantController.

---
+++ Sources/Submodules/SwiftyStore/Controllers/SwiftyStore.MerchantController.swift
@@ -0,0 +1,80 @@
+//
+//  SwiftyStore.MerchantController.swift
+//  https://github.com/Pimine/PimineSDK
+//
+//  This code is distributed under the terms and conditions of the MIT license.
+//  Copyright (c) 2020 Pimine
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import StoreKit
+import SVProgressHUD
+import SwiftyStoreKit
+
+extension SwiftyStore {
+open class MerchantController: RestoringController {
+    
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        productInterfaceController.fetchDataIfNecessary()
+    }
+    
+    // MARK: - Public API
+    
+    public func purchaseProduct(_ product: Product) {
+        productInterfaceController.purchaseProduct(product)
+    }
+    
+    open override func productInterfaceController(
+        _ controller: ProductInterfaceController,
+        didCommitPurchaseWith result: ProductInterfaceController.CommitPurchaseResult
+    ) {
+        SVProgressHUD.dismiss()
+        guard case let .failure(error) = result else { return }
+        
+        switch error {
+        case .userCancelled:
+            break
+        case .receiptError(let error):
+            handleReceiptError(error)
+        case .purchaseNotAvailable:
+            PMAlert.show(message: Messages.productNotAvailable)
+        case .paymentNotAllowed:
+            PMAlert.show(message: Messages.cannotMakePayments)
+        case .paymentInvalid:
+            PMAlert.show(message: Messages.paymentInvalid)
+        case .genericProblem(let error) where error is SKError:
+            let errorCode = (error as! SKError).errorCode
+            PMAlert.show(message: "\(Messages.storeCommunicationError) (Error code: \(errorCode)")
+        case .genericProblem(let error):
+            PMAlert.show(message: "\(Messages.error). \(error.localizedDescription)")
+        }
+    }
+    
+    private func handleReceiptError(_ error: ReceiptError) {
+        switch error {
+        case .noReceiptData:
+            PMAlert.show(message: Messages.noReceiptData)
+        case .networkError(error: let error):
+            PMAlert.show(message: "\(Messages.verificationNetworkProblem): \(error.localizedDescription)")
+        default:
+            PMAlert.show(message: "\(Messages.verificationFailed): \(error.localizedDescription)")
+        }
+    }
+}}
--- Sources/Submodules/SwiftyStore/Internal/SwiftyStore.Messages.swift
+++ Sources/Submodules/SwiftyStore/Internal/SwiftyStore.Messages.swift
@@ -36,11 +36,28 @@ struct Messages {
     static let contactSupport = "If the problem persists, contact support"
     static let somethingWentWrong = "Something went wrong. \(tryAgain). \(contactSupport)."
     
-    // MARK: - Messages related to receipt verification
+    // MARK: - MEssages related to commit purchases
     
-    static let wrongProductType = "\(error). Wrong product type."
+    static let productNotAvailable = "The product is not available in the current storefront."
     
-    // MARK: - Messages related to purchases restoring
+    static let paymentInvalid = "One of the payment parameters was not recognized by the App Store."
+    static let storeCommunicationError = "There was an error communicating with the iTunes Store."
+    
+    static let notAuthorized = "You are not authorized to make payments."
+    static let purchasesMayBeRestricted = "In-App Purchases may be restricted on your device."
+    static let cannotMakePayments = "\(notAuthorized) \(purchasesMayBeRestricted)"
+    
+    // MARK: - Messages related to verifying receipt
+    
+    static let verificationFailed = "Receipt verification failed"
+    
+    static let wrongProductType = "\(verificationFailed). Wrong product type."
+    
+    static let verificationNetworkProblem = "Network error while verifying receipt"
+    
+    static let noReceiptData = "\(verificationFailed). No receipt data. Try again."
+    
+    // MARK: - Messages related to restore
     
     static var restored = "All transactions have been successfully restored."
     static var nothingToRestore = "There are no transactions that could be restored."
